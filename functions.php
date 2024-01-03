efe6c7f76e521bf0c19a5f05ce94654b6b22e5d9
rossjohneast@hotmail.com <rossjohneast@hotmail.com>
2024-01-03 15:27:07 +0000

    Remove uneeded code, register the child style.css in the editor

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
+// Enqueue child styles
 function fse_child_styles() {
+	
 	wp_enqueue_style( 'fse-child-style', get_stylesheet_uri() );
+
 }
 add_action( 'wp_enqueue_scripts', 'fse_child_styles' );
 
@@ -19,58 +22,37 @@ add_action( 'wp_enqueue_scripts', 'fse_child_styles' );
 // add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 11 );
 
 
+//A custom generated SCSSPHP style sheet, this files exists & is written to by customizer
 function enqueue_customizer_scssphp_styles() {
-	//A custom generated SCSSPHP style sheet, this files exists & is written to by customizer
+	
 	wp_enqueue_style( 'wp-zenflow-child-bs-theme-options', get_stylesheet_directory_uri() . '/options-generated-bootstrap-stylesheet.css' );
- }
- 
- //Import parent style sheets
- add_action( 'wp_enqueue_scripts', 'enqueue_customizer_scssphp_styles', 4);
+
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_customizer_scssphp_styles', 4);
 
 
-//  function zenflow5_theme_add_editor_styles() {
-// 	add_editor_style( get_theme_file_uri( '/options-generated-bootstrap-stylesheet.css' ) );
-//   }
-//   add_action( 'admin_init', 'zenflow5_theme_add_editor_styles' );
-
-
-//   function swsg_theme_add_editor_styles() {
-//     // add_editor_style( get_template_directory_uri() . '/build/admin/style-editor.css' );
-//     add_editor_style( get_theme_file_uri( '/options-generated-bootstrap-stylesheet.css' )  );
-// }
-// add_action( 'admin_init', 'swsg_theme_add_editor_styles', 20 );
-
-
-// add_action( 'admin_init', 'generate_child_setup' );
-// function generate_child_setup() 
-// {
-// 	add_editor_style( get_stylesheet_directory_uri() . '/options-generated-bootstrap-stydddlesheet.css' );
-// }
-
-
-
-// add_theme_support('editor-styles');
-// /**
-//  * Registers an editor stylesheet for the theme.
-//  */
-// function wpdocs_theme_add_editor_styles() {
-//     add_editor_style( '/options-generated-bootstrap-stylesheet.css' );
-// }
-// add_action( 'admin_init', 'wpdocs_theme_add_editor_styles' );
-#
 /**
  * Registers an editor stylesheet for the theme.
  */
-// function zenflow5_add_editor_styles() {
-//     add_editor_style( get_template_directory_uri() . '/options-generated-bootstrap-stylesheet.css' );
-// }
-// add_action( 'admin_init', 'zenflow5_add_editor_styles', 20 );
-
-
 function zenflow5_add_editor_styles() {
     add_editor_style( get_stylesheet_directory_uri() . '/options-generated-bootstrap-stylesheet.css' );
 }
 add_action( 'admin_init', 'zenflow5_add_editor_styles', 20 );
+
+
+/**
+ * Registers the child style.css editor stylesheet for the theme.
+ */
+function enqueue_child_theme_editor_style() {
+    // Get the child theme stylesheet URL
+    $child_theme_css_url = get_stylesheet_directory_uri() . '/style.css';
+
+    // Enqueue the child theme stylesheet for the editor
+    wp_enqueue_style('child-theme-editor-style', $child_theme_css_url);
+}
+
+add_action('enqueue_block_editor_assets', 'enqueue_child_theme_editor_style');
+
 
 
 ?>
