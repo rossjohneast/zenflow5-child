c96cc1c0a6a46b5b76084d7a9cf5c3eb62d7dc74
rossjohneast@hotmail.com <rossjohneast@hotmail.com>
2024-01-04 11:07:52 +0000

    Code tidy

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 
-// Enqueue child styles
+/**
+ * Enqueue child styles
+ */
 function fse_child_styles() {
 	
 	wp_enqueue_style( 'fse-child-style', get_stylesheet_uri() );
@@ -9,7 +11,10 @@ function fse_child_styles() {
 add_action( 'wp_enqueue_scripts', 'fse_child_styles' );
 
 
-//A custom generated SCSSPHP style sheet, this files exists & is written to by customizer
+
+/**
+ * A custom generated SCSSPHP style sheet, this files exists & is written to by customizer
+ */
 function enqueue_customizer_scssphp_styles() {
 	
 	wp_enqueue_style( 'wp-zenflow-child-bs-theme-options', get_stylesheet_directory_uri() . '/options-generated-bootstrap-stylesheet.css' );
@@ -18,28 +23,32 @@ function enqueue_customizer_scssphp_styles() {
 add_action( 'wp_enqueue_scripts', 'enqueue_customizer_scssphp_styles', 4);
 
 
+
 /**
  * Registers an editor stylesheet for the theme.
  */
 function zenflow5_add_editor_styles() {
+    
     add_editor_style( get_stylesheet_directory_uri() . '/options-generated-bootstrap-stylesheet.css' );
+
 }
 add_action( 'admin_init', 'zenflow5_add_editor_styles', 20 );
 
 
+
 /**
- * Registers the child style.css editor stylesheet for the theme.
+ * Registers the editor stylesheet (Child Themes CSS: style.css)
  */
 function enqueue_child_theme_editor_style() {
+    
     // Get the child theme stylesheet URL
     $child_theme_css_url = get_stylesheet_directory_uri() . '/style.css';
 
     // Enqueue the child theme stylesheet for the editor
     wp_enqueue_style('child-theme-editor-style', $child_theme_css_url);
+
 }
-
 add_action('enqueue_block_editor_assets', 'enqueue_child_theme_editor_style');
-
 
 
 ?>
