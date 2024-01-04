efd3c90b9184769e9449b88e59fe351e488bca91
rossjohneast@hotmail.com <rossjohneast@hotmail.com>
2024-01-04 11:04:11 +0000

    Removed dequeuing of CSS that wasnt needed

--- functions.php
+++ functions.php
@@ -9,19 +9,6 @@ function fse_child_styles() {
 add_action( 'wp_enqueue_scripts', 'fse_child_styles' );
 
 
-//Dequeue the generated parent stylesheet
-// function child_enqueue_styles() {
-
-// 	// dequeue the Twenty Twenty-One parent style
-// 	wp_dequeue_style( 'zenflow5_style_bs_gen' );
-    
-// 	// Theme stylesheet
-// 	//wp_enqueue_style( 'child-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version') );
-
-// }
-// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 11 );
-
-
 //A custom generated SCSSPHP style sheet, this files exists & is written to by customizer
 function enqueue_customizer_scssphp_styles() {
 	
