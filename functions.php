3ca1fef9641d131b22ac0988995b4ece45bdfa42
rossjohneast@hotmail.com <rossjohneast@hotmail.com>
2024-02-01 18:15:26 +0000

    Updated css ref

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  */
 function fse_child_styles() {
 	
-	wp_enqueue_style( 'fse-child-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'zenflow5-child-style', get_stylesheet_uri() );
 
 }
 add_action( 'wp_enqueue_scripts', 'fse_child_styles' );
